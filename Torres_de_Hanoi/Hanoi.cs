86d255b61c846025722e00e422019ccee4d26745
RobertoMati <robertomatillaaugustinus@gmail.com>
2022-02-21 20:09:32 +0100

    Clase Pila implementada

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,9 +8,35 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
-        /*TODO: Implementar métodos*/
+        //Creación variables
+        private int movimientos;
+        private int discos;
+
+        //Métodos
         public void mover_disco(Pila a, Pila b)
         {
+            //Si la pila b está vacía, se mueve el disco a la pila a
+            if (b.isEmpty() == true)
+            {
+                movimientoDisco(a, b);
+            }
+            else if (a.isEmpty() == false)
+            {
+                movimientoDisco(b, a);
+            }
+            else
+            {
+                Disco discoA = b.Elementos[b.Top];
+                Disco discoB = a.Elementos[a.Top];
+            }
+
+        }
+
+        public void movimientoDisco(Pila a, Pila b)
+        {
+            Disco disco = a.pop();
+            Console.WriteLine("El disco se ha movido" + disco.Valor);
+            b.push(disco);
 
         }
 
--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -8,36 +8,74 @@ namespace Torres_de_Hanoi
 {
     class Pila
     {
+        //PROPIEDADES
+        //------------------------------------------
+        //Propiedad Size
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+        
+        //Propiedad Top
+        public int Top { get { return Elementos.Count; } set { } }
 
-        /* TODO: Implementar métodos */
+       //Propiedad Elementos (Lista)
+        public List<Disco> Elementos { get; set; }
+        //------------------------------------------
+        
+
+        //MÉTODOS
+        //------------------------------------------
+
+        //---------------------------
+        //Constructor de la clase
         public Pila()
         {
-
+            //Creamos nueva lista
+            Elementos = new List<Disco>();
+            //Inicializamos Size a 0
+            Size = 0;
         }
+        //---------------------------
 
+        //---------------------------
+        //Colocar disco en la pila
         public void push(Disco d)
         {
-
+            //Añadimos a la lista el Disco d (Añadimos un disco a la pila)
+            Elementos.Add(d);
+            //Incrementamos el valor de Size (El disco añadido)
+            Size++;
         }
+        //---------------------------
 
+        //---------------------------
+        //Extraer disco de la pila
         public Disco pop()
         {
-            return null;
-        }                
+            //Extraemos el disco de la pila
+            //El -1 Debe estar porque el Elementos.Count devuelve 3, y como se registran las posiciones (0,1,2), se saldría y da error
+            Disco d = Elementos[Elementos.Count - 1];
+            Elementos.RemoveAt(Elementos.Count - 1);
+            Size--;
+            return d;
+        }
+        //---------------------------
 
+        //---------------------------
+        //Informa si la pila está vacía o no
         public bool isEmpty()
         {
-            return true;
+            //Si el tamaño es 0, está vacío, por lo que devuelve true
+            if(Size == 0)
+            {
+                return true;
+            }
+
+            //Si no, devuelve falso.
+            else
+            {
+                return false;
+            }
         }
+        //---------------------------
 
     }
 }
